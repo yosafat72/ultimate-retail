6bddddf9d94a667f9590881d998ac680808942f5
Yosafat Hermawan S <Yosafat.suryana@mncbank.co.id>
2022-07-20 18:05:02 +0700

    add show randomID in form input user

--- src/main/java/com/yohesu/ultimate_retail/controller/master/UserController.java
+++ src/main/java/com/yohesu/ultimate_retail/controller/master/UserController.java
@@ -1,14 +1,28 @@
 package com.yohesu.ultimate_retail.controller.master;
 
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Random;
+
+import javax.servlet.ServletContext;
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import com.yohesu.ultimate_retail.model.UserModel;
 import com.yohesu.ultimate_retail.service.UserService;
 
 @Controller
 public class UserController {
+
+    private ServletContext servletContext;
+
+    public UserController(ServletContext servletContext) {
+        this.servletContext = servletContext;
+    }
 
     @Autowired
     private UserService userService;
@@ -17,6 +31,18 @@ public class UserController {
     public String viewUserPage(Model model) {
         model.addAttribute("listOfUsers", userService.findAll());
         return "fragments/master/user/list-user";
+    }
+
+    @GetMapping("/master/user/view-add-user")
+    public String viewAddUserPage(Model model) {
+        Random rand = new Random();
+        int randomValue = rand.nextInt(1000);
+        String dateNow = new SimpleDateFormat("yyMMdd").format(Calendar.getInstance().getTime());
+        String combineValue = dateNow + randomValue;
+        UserModel user = new UserModel();
+        user.setIdUser(Long.parseLong(combineValue));
+        model.addAttribute("user", user);
+        return "fragments/master/user/add-user";
     }
 
 }
